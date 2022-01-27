53315e133af3bb604e72dfe4be1703e6ca754def
yewyewXD <yewyew6933@gmail.com>
2022-01-27 14:15:53 +0800

    rm useless stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import "../styles/global.scss";
-import { MoralisProvider } from "react-moralis";
+// import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider
-      appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
-      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
+    // <MoralisProvider
+    //   appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
+    //   serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER_URL}
+    // >
+    <Component {...pageProps} />
+    // </MoralisProvider>
   );
 }
 
