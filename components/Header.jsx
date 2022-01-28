04e6131e5ec6005cc3f5c510e1d2234d29041e50
yewyewXD <yewyew6933@gmail.com>
2022-01-28 19:43:52 +0800

    fix header

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,21 +5,30 @@ const Header = () => {
   return (
     <header className="py-7">
       <div className="container mx-auto">
-        <div className="grid grid-cols-3 w-full">
+        <div className="flex justify-between items-center w-full">
           <div className="flex justify-start items-center">
-            <Image src="/images/Brand.svg" height={58} width={282} alt="" />
+            <Image
+              src="/images/Brand.svg"
+              className="pointer-events-none"
+              height={58}
+              width={282}
+              alt=""
+            />
             {/* <Image src="/images/Logo.svg" height={58} width={71.5} alt="" /> */}
           </div>
 
           <nav className="flex justify-center items-center gap-8">
+            <span>Certification</span>
             <span>Portfolio</span>
             <span>Services</span>
-            <span>About</span>
+            <span>À propos de moi</span>
           </nav>
 
-          <div className="flex justify-end items-center">
-            <button className="p-5 bg-lightPurple">Book a free call</button>
-          </div>
+          {/* <div className="flex justify-end items-center">
+            <button className="py-3 px-6 bg-darkPurple font-semibold text-lg transition duration-200 hover:opacity-80">
+              Réserve ton appel GRATUIT !
+            </button>
+          </div> */}
         </div>
       </div>
     </header>
