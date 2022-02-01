7058042dd94e4f5bdb8ba8b46a6b73d2b269eaeb
yewyewXD <yewyew6933@gmail.com>
2022-02-01 14:39:52 +0800

    add roboto

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,13 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&family=Roboto:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
