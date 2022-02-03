9287199631fc975dc18a4a010870cbd3766a828e
yewyewXD <yewyew6933@gmail.com>
2022-02-03 22:37:21 +0800

    responsive footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,24 +4,28 @@ import socials from "../json/socials.json";
 
 const Footer = () => {
   return (
-    <footer className="py-8 flex justify-center items-center">
-      <span>&copy; 2022 - ZoeeCopywriter.com - All Rights Reserved</span>
-      {socials.map((social, index) => (
-        <a
-          className="ml-3 flex items-center transition duration-200 hover:opacity-80"
-          key={`socialFooter${index}`}
-          href={social.link}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src={`/images/Social${social.name}.png`}
-            height={22}
-            width={22}
-            alt=""
-          />
-        </a>
-      ))}
+    <footer className="py-8 flex md:flex-row flex-col justify-center items-center">
+      <span className="text-center">
+        &copy; 2022 - ZoeeCopywriter.fr - All Rights Reserved
+      </span>
+      <div className="flex items-center md:mt-0 mt-2">
+        {socials.map((social, index) => (
+          <a
+            className="ml-3 flex items-center transition duration-200 hover:opacity-80"
+            key={`socialFooter${index}`}
+            href={social.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src={`/images/Social${social.name}.png`}
+              height={22}
+              width={22}
+              alt=""
+            />
+          </a>
+        ))}
+      </div>
     </footer>
   );
 };
